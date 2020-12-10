e441ba1d8dbbb749db0b695d83020e0b00978ab6
winbigcoms <bigcoms6290@gmail.com>
2020-12-10 11:33:53 +0900

    feat app

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View,Text,StyleSheet}from 'react-native';
+
+const Header= ({title})=>{
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header:{
+    width : `100%`,
+    height:90
+  },
+  headerTitle:{
+
+  }
+})
+
+export default Header
